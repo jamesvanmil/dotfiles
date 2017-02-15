3c4c66c98a309b1ddf0ea83c9ffdc78b50ff8a00
James Van Mil <james.vanmil@gmail.com>
2017-02-15 09:25:42 -0500

    update

--- copy_files.sh
+++ copy_files.sh
@@ -2,10 +2,12 @@
 
 cp ~/.bashrc .bashrc
 cp ~/.bash_profile .bash_profile
+cp ~/.bash_osx .bash_osx
 cp ~/.vimrc .vimrc
 cp ~/.gvimrc .gvimrc
 cp ~/.gitconfig .gitconfig
 cp ~/.gitignore_global .gitignore_global
+cp ~/.git-completion.bash .git-completion.bash
 cp ~/.aspell.en.pws .aspell.en.pws
 cp ~/.tmux.conf .tmux.conf
 mkdir ./.vim/
---
+++ install_files.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+cp .bashrc ~/.bashrc
+cp .bash_profile ~/.bash_profile
+cp .bash_osx ~/.bash_osx
+cp .vimrc ~/.vimrc
+cp .gvimrc ~/.gvimrc
+cp .gitconfig ~/.gitconfig
+cp .gitignore_global ~/.gitignore_global
+cp .git-completion.bash ~/.git-completion.bash
+cp .aspell.en.pws ~/.aspell.en.pws
+cp .tmux.conf ~/.tmux.conf 
+mkdir ./.vim/
+cp -rf ./.vim/colors/ ~/.vim/colors/ 
+cp -rf ./.tmuxinator/ ~/.tmuxinator/ 
